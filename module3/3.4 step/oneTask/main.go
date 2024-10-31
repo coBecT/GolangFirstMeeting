e3e328f5155f6e4c583ec0c4b1cd376dd12c4f50
origin <dimadomokurov@yandex.ru>
2024-10-31 19:49:18 +0300

    add module 2,3

--- module3/3.4 step/oneTask/main.go
+++ module3/3.4 step/oneTask/main.go
@@ -1,44 +1,42 @@
-//package oneTask
-//
-//import (
-//	"fmt"
-//)
-//
-//func main() {
-//	value1, value2, operation := readTask() // исходные данные получаются с помощью этой функции
-//	// все полученные значения имеют тип пустого интерфейса
-//	switch v := value1.(type) {
-//	case float64:
-//		switch b := value2.(type) {
-//		case float64:
-//			switch c := operation.(type) {
-//			case string:
-//				if string(c) == "s" {
-//				}
-//				switch operation {
-//				case "+":
-//					res := value1.(float64) + value2.(float64)
-//					fmt.Printf("%.4f", res)
-//				case "-":
-//					res := value1.(float64) - value2.(float64)
-//					fmt.Printf("%.4f", res)
-//				case "*":
-//					res := value1.(float64) * value2.(float64)
-//					fmt.Printf("%.4f", res)
-//				case "/":
-//					res := value1.(float64) / value2.(float64)
-//					fmt.Printf("%.4f", res)
-//				}
-//			default:
-//				fmt.Print("неизвестная операция")
-//				break
-//			}
-//		default:
-//			fmt.Printf("value=%v: %T", value2, b)
-//			break
-//		}
-//	default:
-//		fmt.Printf("value=%v: %T", value1, v)
-//		break
-//	}
-//}
+package oneTask
+
+func main() {
+	//value1 := readTask() // исходные данные получаются с помощью этой функции
+	//// все полученные значения имеют тип пустого интерфейса
+	//switch v := value1.(type) {
+	//case float64:
+	//	switch b := value2.(type) {
+	//	case float64:
+	//		switch c := operation.(type) {
+	//		case string:
+	//			if string(c) == "s" {
+	//			}
+	//			switch operation {
+	//			case "+":
+	//				res := value1.(float64) + value2.(float64)
+	//				fmt.Printf("%.4f", res)
+	//			case "-":
+	//				res := value1.(float64) - value2.(float64)
+	//				fmt.Printf("%.4f", res)
+	//			case "*":
+	//				res := value1.(float64) * value2.(float64)
+	//				fmt.Printf("%.4f", res)
+	//			case "/":
+	//				res := value1.(float64) / value2.(float64)
+	//				fmt.Printf("%.4f", res)
+	//			}
+	//		default:
+	//			fmt.Print("неизвестная операция")
+	//			break
+	//		}
+	//	default:
+	//		fmt.Printf("value=%v: %T", value2, b)
+	//		break
+	//	}
+	//default:
+	//	fmt.Printf("value=%v: %T", value1, v)
+	//	break
+	//}
+}
+
+//func readTask(){fmt.Println("hello")}
